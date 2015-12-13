eaf8370b2fa327bd271bb457f2d784b152330eee
Jason Seminara <jseminara@gmail.com>
2015-12-13 17:43:07 -0500

    created the restful api

--- cattr/urls.py
+++ cattr/urls.py
@@ -17,8 +17,30 @@ Including another URLconf
 from django.conf.urls import patterns,include,url
 from django.contrib import admin
 from django.contrib.auth import views
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
 
 admin.autodiscover()
+
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+
+
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
@@ -32,4 +54,6 @@ urlpatterns = [
     url(r'^logout/$','django.contrib.auth.views.logout',
       {'next_page':'cattr_home'},
       name='cattr_logout'),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^', include(router.urls)),
 ]
