eb4230f40ac24fccb8d3eb41f67684de8fb88861
Jason Seminara <jseminara@gmail.com>
2015-12-13 22:58:17 -0500

    added grayscale layout and init design
    restyled
    
    gst
    
    updated styles

---
+++ app/scripts/controllers/register.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cattrApp.controller:RegisterCtrl
+ * @description
+ * # RegisterCtrl
+ * Controller of the cattrApp
+ */
+angular.module('cattrApp')
+  .controller('RegisterCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
