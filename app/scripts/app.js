0de60b6ef269762ebd2c35f7b1c4114b7f8f7bf8
Jason Seminara <jseminara@gmail.com>
2015-12-13 20:40:30 -0500

    scaffolded angular

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name cattrApp
+ * @description
+ * # cattrApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('cattrApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cattrApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cattrApp
+ */
+angular.module('cattrApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
