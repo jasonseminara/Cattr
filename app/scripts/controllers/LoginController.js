9005cf5f27f0a675aa0bcbeed2efba40329dcfe5
Jason Seminara <jseminara@gmail.com>
2015-12-15 15:33:59 -0500

    finished login

--- app/scripts/controllers/LoginController.js
+++ app/scripts/controllers/LoginController.js
@@ -16,43 +16,7 @@ cattr
       popupOptions: { width: 580, height: 400 }
     });
 
-    // Google
-    $authProvider.google({
-      url: '/auth/google',
-      authorizationEndpoint: 'https://accounts.google.com/o/oauth2/auth',
-      redirectUri: window.location.origin,
-      requiredUrlParams: ['scope'],
-      optionalUrlParams: ['display'],
-      scope: ['profile', 'email'],
-      scopePrefix: 'openid',
-      scopeDelimiter: ' ',
-      display: 'popup',
-      type: '2.0',
-      popupOptions: { width: 452, height: 633 },
-      clientId: '701518223603-i2era5ipcsil3gbh2h0prhlpvkmle1n7.apps.googleusercontent.com'
-    });
 
-    // GitHub
-    $authProvider.github({
-      url: '/auth/github',
-      authorizationEndpoint: 'https://github.com/login/oauth/authorize',
-      redirectUri: window.location.origin,
-      optionalUrlParams: ['scope'],
-      scope: ['user:email'],
-      scopeDelimiter: ' ',
-      type: '2.0',
-      popupOptions: { width: 1020, height: 618 }
-    });
-    $authProvider.oauth1({
-      url: '/login',
-      authorizationEndpoint: '/login',
-      redirectUri: window.location.origin,
-      optionalUrlParams: ['scope'],
-      scope: ['user:email'],
-      scopeDelimiter: ' ',
-      type: '2.0',
-      popupOptions: { width: 1020, height: 618 }
-    });
 
   }])
   .controller('LoginController', function($scope, $auth) {
