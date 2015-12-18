1947ff71afaced23c6725a9f0f6992e6deacbc0b
Jason Seminara <jseminara@gmail.com>
2015-12-17 22:04:58 -0500

    started flask project

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask
-app = Flask(__name__)
+import os
 
-@app.route("/")
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+
+
+@app.route('/')
 def hello():
-    return "Hello World!"
+  return os.environ['APP_SETTINGS']
+
 
 @app.route('/<name>')
 def hello_name(name):
-    return "Hello {}!".format(name)
+  return "Hello {}!".format(name)
 
 if __name__ == '__main__':
     app.run()
+
--- main/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def home(req):
-  return render(req,'main/home.html',{
-    'message':"Sup Bro"
-  })
