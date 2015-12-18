a892479258c8c263246beed35890d8fe2454ce75
Jason Seminara <jseminara@gmail.com>
2015-12-18 00:38:11 -0500

    moved all NG files to static and removed any rendering from server

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 from flask.ext.sqlalchemy import SQLAlchemy
 import os,requests,json
 
-app = Flask(
-  __name__,
-  template_folder='app',
-  static_folder='/app',
-  static_url_path='/app',
-  )
+app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+
 db = SQLAlchemy(app)
+
+
 
 from models import Result
 
+
+
+print(app.url_map)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -29,7 +31,28 @@ def index():
             errors.append(
                 "Unable to get URL. Please make sure it's valid and try again."
             )
-    return render_template('index.html', errors=errors, results=results)
+    return app.send_static_file('index.html')
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('static/js', path)
+
+@app.route('/scripts/<path:path>')
+def send_scripts(path):
+    return send_from_directory('static/scripts', path)
+
+@app.route('/images/<path:path>')
+def send_images(path):
+    return send_from_directory('static/images', path)
+
+@app.route('/styles/<path:path>')
+def send_styles(path):
+    return send_from_directory('static/styles', path)
+
+@app.route('/views/<path:path>')
+def send_views(path):
+    return send_from_directory('static/views', path)
+
 
 
 if __name__ == '__main__':
